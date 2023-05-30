c11f3fd02526edf75607c845d5074c3713e08c63
unknown <princeparco12@gmail.com>
2023-05-30 18:07:57 +0800

    New Update

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Prince Charm C. Parco</span> I'm a Junior Web Developer, I just recently graduated to KODEGO Bootcamp and I am eager to learn and explore more in a  Web Developer."
+        "<span>Hi, I am Prince Charm C. Parco</span> a passionate and dedicated Junior Web Developer. With a strong foundation in web development and a desire to learn and grow, I strive to create engaging and user-friendly web experiences. I just recently graduated to Kodego Bootcamp and During my time as a student of Kodego, I have created on some of projects that have helped me enhance my skills and gain practical experience."
       ],
       typeSpeed: 7,
       contentType: 'html'
